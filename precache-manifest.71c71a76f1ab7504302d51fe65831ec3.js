bfe4bf4433f4aff8d071924f403c2347fd002e87
Feddle <sepptee@outlook.com>
2019-03-28 18:29:24 +0200

    Updates

---
+++ precache-manifest.71c71a76f1ab7504302d51fe65831ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d067beaab7331c2c9f82",
+    "url": "/pokemon-store/static/js/runtime~main.78f45083.js"
+  },
+  {
+    "revision": "7d51fc02ade0ae3d33af",
+    "url": "/pokemon-store/static/js/main.2ee4a490.chunk.js"
+  },
+  {
+    "revision": "0a0cbda9e1ecdadf78d2",
+    "url": "/pokemon-store/static/js/2.4df385cd.chunk.js"
+  },
+  {
+    "revision": "7d51fc02ade0ae3d33af",
+    "url": "/pokemon-store/static/css/main.e0f55d4c.chunk.css"
+  },
+  {
+    "revision": "f6886e8884e294bb34257e44c94064c3",
+    "url": "/pokemon-store/index.html"
+  }
+];
